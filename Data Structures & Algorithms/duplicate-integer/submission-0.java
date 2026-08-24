5a735d97decb4f43b22de4dd25549ea8cf31bd0f
shivamrajiwade333 <shivamrajiwade1086@gmail.com>
2026-08-24 13:17:28 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static boolean hasDuplicate(int nums[]) {
+        HashMap <Integer , Integer > map = new HashMap <>(); 
+      for(int i = 0 ; i<nums.length ; i++){
+         if(map.containsKey(nums[i])){
+            return true ; 
+         }
+         map.put(nums[i], 1);
+      }
+      return false ;
+   }
+}
